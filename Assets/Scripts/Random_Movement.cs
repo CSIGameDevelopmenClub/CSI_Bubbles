1b46d3d1b8cba287d03fdc222c6c00891622674e
Stanisrawr <stlesk@GMAIL.COM>
2015-10-29 23:48:42 -0400

    Unity Game
    Added the code worked on during club meetings

---
+++ Assets/Scripts/Random_Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Random_Movement : MonoBehaviour {
+
+	public static float xMov, yMov, deciderX, deciderY;
+	// Use this for initialization
+	void Start () {
+	deciderX = Random.Range (-1.0f, 1.0f);
+		deciderY = Random.Range (-1.0f, 1.0f);
+			if (deciderX < 0.0f) 
+			xMov = Random.Range (-70.0f, -30.0f);
+		    else 
+			xMov = Random.Range (30.0f, 70.0f);
+		
+//		    if (deciderY < 0.0f) 
+//			yMov = Random.Range (-7.0f, -3.0f);
+//		else
+//			yMov = Random.Range (3.0f, 7.0f);
+
+			
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (xMov, yMov) * Time.deltaTime;
+
+
+	}
+
+
+}
---
+++ Assets/Scripts/WallCollision.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCollision : MonoBehaviour {
+	float valToChange;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("WORK");
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		Debug.Log ("Collided!");
+		if (gameObject.tag == "West" || gameObject.tag == "East") {
+			other.GetComponent<Rigidbody2D>().velocity = new 
+				Vector2(other.GetComponent<Rigidbody2D>().velocity.x*-1,
+				        other.GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+}
