4df116210c21dc9df6f184f8b3c4e5a50fe75fa9
GabrielGoldstein <gabriel.goldstein@cix.csi.cuny.edu>
2015-11-12 16:22:00 -0500

    Red Ball movement

---
+++ Assets/Assets/Scripts/Random_Movement.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Random_Movement : MonoBehaviour {
+
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		delay(3.0f);
+		Debug.Log ("Delay finish");
+		randMovi ();
+		Debug.Log ("MovementPerformed");
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	IEnumerator delay(float sec)
+	{
+		yield return new WaitForSeconds (sec);
+	}
+
+	void randMovi ()
+	{
+		float randX = Random.Range (-10.0f, 10.0f);
+		float randY = Random.Range (-10.0f, 10.0f);
+
+		if (randX<=0)
+		{
+			gameObject.GetComponent<Rigidbody2D>().AddForce((new Vector2(Random.Range (-10.0f,-3.0f),0)) * speed * Time.deltaTime);
+		}
+		else
+		{
+			gameObject.GetComponent<Rigidbody2D>().AddForce((new Vector2(Random.Range (3.0f,10.0f),0)) * speed * Time.deltaTime);
+		}
+		if (randY<=0)
+		{
+			gameObject.GetComponent<Rigidbody2D>().AddForce((new Vector2(0,Random.Range (-10.0f,-3.0f))) * speed * Time.deltaTime);
+		}
+		else
+		{
+			gameObject.GetComponent<Rigidbody2D>().AddForce((new Vector2(0,Random.Range (3.0f,10.0f))) * speed * Time.deltaTime);
+		}
+				                     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Assets/Scripts/WallCollision.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCollision : MonoBehaviour {
+	float valToChange;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("WORK");
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		Debug.Log ("Collided!");
+		if (gameObject.tag == "West" || gameObject.tag == "East") {
+			other.GetComponent<Rigidbody2D>().velocity = new 
+				Vector2(other.GetComponent<Rigidbody2D>().velocity.x*-1,
+				        other.GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if (gameObject.tag == "North" || gameObject.tag == "South") {
+			other.GetComponent<Rigidbody2D>().velocity = new 
+				Vector2(other.GetComponent<Rigidbody2D>().velocity.x,
+				        other.GetComponent<Rigidbody2D>().velocity.y*-1);
+		}
+	}
+}
