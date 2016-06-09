838963c7a1a11fccff7fe9305cb1a01ff0dca616
Stanislav <stlesk@GMAIL.COM>
2016-06-09 17:43:23 -0400

    make it pretty
    Changed sprites and created script for a random background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+	public GameObject[] bg;
+	public GameObject canvas;
+	int rand;
+	// Use this for initialization
+	void Start () {
+		rand = Random.Range (0, 4);
+		var createBG=Instantiate (bg [rand]) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
